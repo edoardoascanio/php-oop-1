1b94c3389eabe56181a22fd588f797f9042dcef4
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-06-14 19:45:53 +0200

    add method

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 
 include "movie.php";
 
-$film1 = new Movie("Terminator", "The Terminator", "English", "4");
-$film2 = new Movie("Rocky", "Rocky", "English", "4" );
+$film1 = new Movie("Terminator", "The Terminator", "English", "");
+$film2 = new Movie("Rocky", "Rocky", "English", "" );
 
 var_dump($film1);
 var_dump($film2);
+
+$film1->getRandomVote(5);
+$film2->getRandomVote(5);
--- movie.php
+++ movie.php
@@ -6,11 +6,18 @@ class Movie {
     public $language;
     public $vote_average;
 
+
     function __construct($title, $original_title, $language, $vote_average) {
         $this->title = $title;
         $this->original_title = $original_title;
         $this->language = $language;
         $this->vote_average = $vote_average;
         
+    }
+
+    public function getRandomVote($vote_average){
+        $this->vote_average = rand($min = 0, 5);
+
+        return $this->vote_average;
     }
 }
